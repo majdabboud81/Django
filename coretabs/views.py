6736d6871b424ae249aed4e154735a48d181371f
Majd Abboud <majdabboud81@gmail.com>
2018-05-09 12:25:13 +0300

    edit models:reformated

--- coretabs/views.py
+++ coretabs/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("Hi there, this is our new home page.")
 
-def hello(request, name):
-	return HttpResponse('Hello {}'.format(name))
 
+def hello(request, name):
+    return HttpResponse('Hello {}'.format(name))
--- shop/models.py
+++ shop/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Category(models.Model):
-    name=models.CharField(max_length=100)
-    created_at=models.DateTimeField(auto_now_add=True)
-    slug=models.SlugField(unique=True,blank=True)
-    description=models.TextField(blank=True, max_length=256)
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField(blank=True, max_length=256)
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -16,7 +18,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     slug = models.SlugField(unique=True, blank=True)
 
-    category=models.ForeignKey(Category,related_name="products",on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name="products", on_delete=models.CASCADE)
 
     class Meta:
         ordering = ('name',)
